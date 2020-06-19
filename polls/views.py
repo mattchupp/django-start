16e20944d209e96035381d0d7a1ed1ceb6f3f062
mattchupp <mattlchupp@gmail.com>
2020-06-19 07:41:26 -0500

    update detail view

--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,5 @@
 from django.http import Http404
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 
 # Create your views here.
 from django.http import HttpResponse
@@ -16,11 +16,15 @@ def index(request):
   return render(request, 'polls/index.html', context)
 
 def detail(request, question_id):
-  try:
-    question = Question.objects.get(pk=question_id)
-  except Question.DoesNotExist:
-    raise Http404("Question does not exist")
+  # rewrite
+  question = get_object_or_404(Question, pk=question_id)
   return render(request, 'polls/detail.html', {'question': question})
+  # old way
+  # try:
+  #   question = Question.objects.get(pk=question_id)
+  # except Question.DoesNotExist:
+  #   raise Http404("Question does not exist")
+  # return render(request, 'polls/detail.html', {'question': question})
 
 def results(request, question_id):
   response = "you're looking at the results of question %s."
