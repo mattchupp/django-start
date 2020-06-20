57b4f93e35f3d5cf9f92a40eb0e7cc970c0e88f9
mattchupp <mattlchupp@gmail.com>
2020-06-20 07:59:03 -0500

    fixed a typo

--- polls/urls.py
+++ polls/urls.py
@@ -12,6 +12,6 @@ urlpatterns = [
   # /polls/5/results/
   path('<int:question_id>/results', views.results, name='results'),
   # /polls/5/vote/
-  path('<int:question_id>/vote', views.vote, name='vote')
+  path('<int:question_id>/vote/', views.vote, name='vote')
 
 ]
--- polls/views.py
+++ polls/views.py
@@ -28,7 +28,7 @@ def detail(request, question_id):
   # return render(request, 'polls/detail.html', {'question': question})
 
 def results(request, question_id):
-  question = get_question_or_404(Question, pk=question_id)
+  question = get_object_or_404(Question, pk=question_id)
   return render(request, 'polls/results.html', {'question': question})
 
 def vote(request, question_id):
